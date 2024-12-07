2d906d7a359479df037aca06b0b975cd1fbf59c0
Alexander Kohkoinen Morin <102223577+alexanderkohkoinen@users.noreply.github.com>
2024-12-07 22:10:18 +0100

    Day 3 part 2

--- 2024/day3/main.go
+++ 2024/day3/main.go
@@ -1,79 +1,114 @@
 package main
 
 import (
-    "fmt"
-    "github.com/alexanderkohkoinen/aoc/util"
-    "github.com/Knetic/govaluate"
-    // "slices"
+	"fmt"
+	"strings"
+
+	"github.com/Knetic/govaluate"
+	"github.com/alexanderkohkoinen/aoc/util"
+	// "slices"
 )
 
 func main() {
-    util.WelcomeMessage("Day 2");
-    data := util.ReadData("data.txt");
+	util.WelcomeMessage("Day 2")
+	data := util.ReadData("data.txt")
 
-    fmt.Println("Part 1 ->  sum: ", part1(data))
-    fmt.Println("Part 2 ->  sum: ", part2(data))
+	fmt.Println("Part 1 ->  sum: ", part1(data))
+	fmt.Println("Part 2 ->  sum: ", part2(data))
 }
 
 func part1(lists []string) int {
-    // expr := `(mul\((\d{2}\,\d{1}|\d{1}\,\d{1}|\d{1}\,\d{2})\))`
-    expr := `mul\((\d{1,3}\,\d{1,3}\))`
-    sum := 0
+	expr := `mul\((\d{1,3}\,\d{1,3}\))`
+	sum := 0
 
-    for _, row := range lists {
-        result := util.GetMatches(expr, row)
-        fmt.Println(result)
-        eval := BuildAlgoritm(result)
-        fmt.Println(eval)
-        sum += Evalute(eval)
-    }
+	combined := strings.Join(lists, "\n")
+	result := util.GetMatches(expr, combined)
+	eval := BuildAlgoritm(result)
+	sum += Evalute(eval)
 
-    return sum
+	return sum
 }
 
+func part2(lists []string) int {
+	expr := `mul\(\d{1,3}\,\d{1,3}\)|do\(\)|don't\(\)`
+	sum := 0
 
-func part2(arr []string) int {
-    return 2
+	combined := strings.Join(lists, "\n")
+	result := util.GetMatches(expr, combined)
+	eval := BuildAlgoritm2(result)
+	sum += Evalute(eval)
+
+	return sum
 }
-
 
 func BuildAlgoritm(arr []string) string {
-    result := ""
+	result := ""
 
-    for _, eval := range arr {
-        str := util.ReplaceAll(`mul|\(|\)`, eval, "")
-        // fmt.Println(str)
+	for _, eval := range arr {
+		str := util.ReplaceAll(`mul|\(|\)`, eval, "")
+		// fmt.Println(str)
 
-        str = util.ReplaceAll(`,`,str, `*` )
-        if len(result) > 0 {
-            result += " + "
-        }
+		str = util.ReplaceAll(`,`, str, `*`)
+		if len(result) > 0 {
+			result += " + "
+		}
 
-        result += str
-    }
+		result += str
+	}
 
-    return result
+	return result
+}
+
+func BuildAlgoritm2(arr []string) string {
+	result := ""
+	skip := false
+
+	for _, eval := range arr {
+		str := util.ReplaceAll(`mul|\(|\)`, eval, "")
+		switch str {
+		case "don't":
+			skip = true
+		case "do":
+			skip = false
+			continue
+		}
+
+		// Skip current row
+		if skip {
+			continue
+		}
+
+		str = util.ReplaceAll(`,`, str, `*`)
+		if len(result) > 0 {
+			result += " + "
+		}
+
+		result += str
+	}
+
+	// fmt.Println(result)
+	return result
 }
 
 func Evalute(value string) int {
-    expr, err := govaluate.NewEvaluableExpression(value)
-    if err != nil {
-        fmt.Println("Error parsing", err)
-        return 0
-    }
+	expr, err := govaluate.NewEvaluableExpression(value)
+	if err != nil {
+		fmt.Println("Error parsing: ", err)
+		return 0
+	}
 
-    result, err := expr.Evaluate(nil)
-    if err != nil {
-        fmt.Println("Error evaluating", err)
-        return 0
-    }
+	result, err := expr.Evaluate(nil)
+	if err != nil {
+		fmt.Println("Error evaluating: ", err)
+		return 0
+	}
 
-    fmt.Println("Result: ", result)
+	// fmt.Println("Result: ", result)
 
-    num, ok := result.(float64) 
-    if !ok {
-        return 0
-    }
+	num, ok := result.(float64)
+	if !ok {
+		return 0
+	}
 
-    return int(num)
+	return int(num)
 }
--- 2024/day3/main_test.go
+++ 2024/day3/main_test.go
@@ -10,7 +10,7 @@ func TestPart1(t *testing.T) {
         "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
 	}
 
-	expected := 2
+	expected := 161
 	result := part1(rows)
 
 	if result != expected {
@@ -21,15 +21,10 @@ func TestPart1(t *testing.T) {
 
 func TestPart2(t *testing.T) {
 	rows := []string{
-		"7 6 4 2 1",
-		"1 2 7 8 9",
-		"9 7 6 2 1",
-		"1 3 2 4 5",
-		"8 6 4 4 1",
-		"1 3 6 7 9",
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
 	}
 
-	expected := 4
+	expected := 48
 	result := part2(rows)
 
     if result != expected {
