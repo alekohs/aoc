a350843d591df6be18da6cf230d3a27a80039349
Alexander Kohkoinen Morin <102223577+alexanderkohkoinen@users.noreply.github.com>
2024-12-19 14:42:36 +0100

    Added day 5

---
+++ 2024/day5/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	// "errors"
+	"fmt"
+	// "regexp"
+	// "strings"
+
+	// "strings"
+	"github.com/alexanderkohkoinen/aoc/util"
+)
+
+func main() {
+	util.WelcomeMessage("Day 4")
+	data := util.ReadData("data.txt")
+
+	fmt.Println("Part 1 ->  sum: ", part1(data))
+	fmt.Println("Part 2 ->  sum: ", part2(data))
+}
+
+func part1(rows []string) int {
+    return 0
+}
+
+func part2(rows []string) int {
+    return 0
+}
+
---
+++ 2024/day5/main_test.go
@@ -0,0 +1,31 @@
+
+package main
+
+import (
+	"testing"
+	"github.com/alexanderkohkoinen/aoc/util"
+)
+
+func TestPart1(t *testing.T) {
+	data := util.ReadData("data.txt")
+
+	expected := 18
+	result := part1(data)
+
+	if result != expected {
+		t.Fatalf("part1() = %v, want %v", result, expected)
+	}
+}
+
+
+func TestPart2(t *testing.T) {
+	data := util.ReadData("data.txt")
+
+	expected := 9
+	result := part2(data)
+
+	if result != expected {
+		t.Fatalf("part2() = %v, want %v", result, expected)
+	}
+
+}
